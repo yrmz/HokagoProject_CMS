be9477e3c14c59158c50ebb9a925c0a676391940
Rimy <>
2020-03-14 01:03:36 +0900

    新規作成、ルーティング作成

---
+++ frontend/src/client/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from 'client/Component/Layout';
+
+function App() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/client/Container/Document/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Type層
+
+// DOM層
+const Componet: React.FC = props => {
+    return <>
+        <h1>Document List</h1>
+    </>
+}
+
+//Style層
+
+//Container層
+
+export default Componet;
